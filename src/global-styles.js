a7e5dc81521dab059e30d3a977ce800366797a89
Rúben Freitas <emanuelfreitas477@gmail.com>
2020-08-20 09:23:45 +0100

    Initial setup

---
+++ src/global-styles.js
@@ -0,0 +1,21 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+  body {
+    margin: 0;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+      'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+      sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+  
+  html {
+    box-sizing: border-box;
+  }
+  
+  *, *:before, *:after {
+    box-sizing: inherit;
+  }
+  
+`;
